b673090064e466a07c22e2b59ede8f8e4dd202f6
dvld <nikoroberts620@gmail.com>
2019-04-09 23:50:24 -0500

    hid apikey

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const request = require('request');
 const path = require('path');
 const http = require('http');
+const keys = require('./keys');
+// console.log(keys);
+require("dotenv").config();
+const key = keys.mailchimp;
+// console.log(key);
 
 // instantiate express app
 const app = express();
@@ -44,7 +49,7 @@ app.post('/signup', (req, res) => {
     url: 'https://us7.api.mailchimp.com/3.0/lists/7b32c9efd1',
     method: 'POST',
     headers: {
-      Authorization: 'auth 674de983b8b10f63970a364c0dd8e4f2-us7'
+      Authorization: `auth ${key}`
     },
     body: postData
   }
